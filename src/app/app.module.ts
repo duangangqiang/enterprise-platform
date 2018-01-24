fe13ae880721e12a68ca5af1d62dd61788904c36
gangqiang <gangqiang.duan@idsuipai.com>
2018-01-24 09:25:43 +0800

    布局

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MdSidenavModule } from '@angular/material';
+
 import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 
@@ -11,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    MdSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]

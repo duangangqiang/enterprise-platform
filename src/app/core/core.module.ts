622828535a584b9ee27f153e8cc883df19bc42e1
gangqiang <gangqiang.duan@idsuipai.com>
2018-01-24 09:51:28 +0800

    集中处理svg

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,14 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
+import { MdIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 import { MdToolbarModule, MdIconModule, MdButtonModule } from '@angular/material';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+
+import { loadSvgResources } from '../utils/svg.util';
 
 @NgModule({
   imports: [
@@ -26,9 +30,10 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   ]
 })
 export class CoreModule {
-  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+  constructor(@Optional() @SkipSelf() parent: CoreModule, ir: MdIconRegistry, ds: DomSanitizer) {
     if (parent) {
       throw new Error('模块已经存在，不能再次加载！');
     }
+    loadSvgResources(ir, ds);
   }
 }
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { MdIconRegistry } from '@angular/material';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-header',
@@ -11,9 +9,7 @@ export class HeaderComponent implements OnInit {
 
   @Output() toggle = new EventEmitter<void>();
 
-  constructor(iconRegistry: MdIconRegistry, sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon('gifts', sanitizer.bypassSecurityTrustResourceUrl('assets/gifts.svg'));
-  }
+  constructor() {}
 
   ngOnInit() {
   }
